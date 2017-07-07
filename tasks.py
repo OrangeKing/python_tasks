04ff7fcfc4486f9500f451563084d2e616ea9478
Kapica,  Norbert <norbert.kapica@mobica.com>
2017-07-07 16:28:43 +0200

    Task list added as single field list

--- tasks.py
+++ tasks.py
@@ -1,36 +1,50 @@
 #!/usr/bin/env python
 
+#utility import
 from os import system
-from termcolor import cprint
+from termcolor import cprint,colored
 import colorama
 
+#coloring support (for priorities & errors)
 colorama.init()
 
+#global task 'vector'
+tasks = []
+
+#primitive ui
 def printMenu():
     menu_items = ["1 - Create task", "2 - Remove task", "3 - Set priority", \
     "4 - Display sorted by priority", "5 - Display sorted by name"]
-
     print("Select an action to perform:\n ")
     for item in menu_items:
         print(item)
 
 def createTask():
+    global tasks
+    name = raw_input("Task name: ")
+    tasks.append(name)
     pass # TODO
 
 def removeTask():
+    print(colored(tasks.pop(),"blue") + " task has been deleted\n")
     pass # TODO
 
 def setPriority():
     pass # TODO
 
 def displayByPriority():
+    print("Current tasks:")
+    for task in tasks:
+        cprint(task,"blue")
     pass # TODO
 
 def displayByName():
+    print("Current tasks:")
+    for task in sorted(tasks):
+        cprint(task,"blue")
     pass # TODO
 
 def defaultAction():
-    system('clear')
     cprint("No such action available!\n", "red")
     pass
 
@@ -44,11 +58,10 @@ def main():
         if select == "q":
             break
         else:
+            system('clear')
             actions.get(select, defaultAction)()
 main()
 
-#Task item structure
-#Task done/undone
 #Tasks priority
 #Display task format?
 #Display sorted by priority
